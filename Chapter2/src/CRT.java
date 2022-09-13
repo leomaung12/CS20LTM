469b924f2f1f4b7a934df66b2d0bc760340a737e
1100049812 <1100049812@L815WD220029.edu.cbe.ab.ca>
2022-09-13 15:21:50 -0600

    3:21

---
+++ Chapter2/src/CRT.java
@@ -0,0 +1,23 @@
+
+public class CRT {
+
+	public static void main(String[] args) {
+		/*
+		 *3. TestAverages.java
+Calculates test averages
+Leo Thein Maung
+September 13, 2022
+
+4. // The AverageScore displays the average set of scores
+
+5.
+
+6.
+
+7.
+
+		 */
+
+	}
+
+}
