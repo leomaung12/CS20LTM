d33bd87012056692b7fe27d0317cfd4a2304c3d1
1100049812 <1100049812@L815WD220029.edu.cbe.ab.ca>
2022-09-14 13:12:17 -0600

    1:12 PM

--- Chapter2/src/initialsoutput.java
+++ Chapter2/src/initialsoutput.java
@@ -1,9 +1,39 @@
+/*
+Program: ${myInitials}.java          Date: ${4/6/2022}
+Author: Leo Thein Maung
+School: CHHS
+Course: Computer Science 10
+ 
+*/
 
-public class initialsoutput {
+public class initialsoutput 
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		String initials = "" // variable named initials
+				+ "		 |				    ----------                 \r\n"
+				+ "		 |					|			||          | |\r\n"
+				+ "		 |					|			|  |       |  |\r\n"
+				+ "		 |					|			|   |     |   |\r\n"
+				+ "		 |					|			|    |   |    |\r\n"
+				+ "		 |					|			|     | |     |\r\n"
+				+ "		 |					|			|      |      |\r\n"
+				+ "		 -------------		.		|		.		\r\n"
+				+ "		 					";
 
-	}
-
+		 
+		System.out.println(initials); // printing variable initials on screen
+	} 
 }
+/* Screen Dump
+ 		 |				----------                 
+		 |					|			||          | |
+		 |					|			|  |       |  |
+		 |					|			|   |     |   |
+		 |					|			|    |   |    |
+		 |					|			|     | |     |
+		 |					|			|      |      |
+		 -------------.		|		.		
+		 					
+ */
---
+++ Chapter3/src/CRT/CRT.java
@@ -0,0 +1,10 @@
+package CRT;
+
+public class CRT {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
