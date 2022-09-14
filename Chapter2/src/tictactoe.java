2ea583bbb71e84adbf35826b0fe78bf1db8b5990
1100049812 <1100049812@L815WD220029.edu.cbe.ab.ca>
2022-09-14 12:49:14 -0600

    12:49 pm

--- Chapter2/src/tictactoe.java
+++ Chapter2/src/tictactoe.java
@@ -1,9 +1,39 @@
-
+/*
+Program: ${tictactoe}.java          Date: ${9/14/2022}
+Author: Leo Thein Maung
+School: CHHS
+Course: Computer Science 20
+ 
+*/
 public class tictactoe {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		System.out.println("			 |		 |			");
+		System.out.println("			 |		 |			");
+		System.out.println("			 |		 |			");
+		System.out.println("		-----------------------------------");
+		System.out.println("			 |		 |			");
+		System.out.println("			 |	 X    	 |			");
+		System.out.println("			 |		 |			");
+		System.out.println("		-----------------------------------");
+		System.out.println("			 |		 |			");
+		System.out.println("			 |		 |			");
+		System.out.println("			 |		 |			");
 
 	}
 
 }
+/* Screen Dump
+			 	|		 |			
+			 	|		 |			
+			 	|		 |
+	-----------------------------------
+			 	|		 |			
+			 	|	X    |			
+			 	|		 |	
+	-----------------------------------
+			 	|		 |			
+			 	|		 |			
+			 	|		 |
+*/
