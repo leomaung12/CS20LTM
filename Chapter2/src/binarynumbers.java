9e7e93d39b7bd14c13d0633b9b699c3e99f8615b
1100049812 <1100049812@L815WD220029.edu.cbe.ab.ca>
2022-09-14 13:06:49 -0600

    1:06 PM

--- Chapter2/src/binarynumbers.java
+++ Chapter2/src/binarynumbers.java
@@ -1,9 +1,36 @@
-
+/*
+Program: ${binarynumbers}.java          Date: ${9/14/2022}
+Author: Leo Thein Maung
+School: CHHS
+Course: Computer Science 20
+ 
+*/
 public class binarynumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Decimal			Binary");
+		System.out.println("1			     1");
+		System.out.println("2			    10");
+		System.out.println("3			    11");
+		System.out.println("4			   100");
+		System.out.println("5			   101");
+		System.out.println("6			   110");
+		System.out.println("7			   111");
+		System.out.println("8			  1000");
+
 
 	}
 
 }
+/* Screen Dump
+Decimal			Binary
+1			     1
+2			    10
+3			    11
+4			   100
+5			   101
+6			   110
+7			   111
+8			  1000
+
+*/
