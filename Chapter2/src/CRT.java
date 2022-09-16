f2ee4c9f72c8a658a42bf0bafb02be76aeb2cb79
1100049812 <1100049812@L815WD220029.edu.cbe.ab.ca>
2022-09-16 11:16:13 -0600

    11:15AM

--- Chapter2/src/CRT.java
+++ Chapter2/src/CRT.java
@@ -3,18 +3,21 @@ public class CRT {
 
 	public static void main(String[] args) {
 		/*
-		 *3. TestAverages.java
+3. TestAverages.java
 Calculates test averages
 Leo Thein Maung
 September 13, 2022
 
-4. // The AverageScore displays the average set of scores
+4. // The AverageScore displays the average set of scores among students
 
-5.
+5.package gradeCalculator;
 
-6.
+6. static Avergescore
 
-7.
+7. Source code is a collection of computer instructions written with a human-reading programming language, meaning it is typed by the programmer, while the bytecode basically puts together the programmers code.
+
+12. The difference between print() and println() is that print() does not move the cursor to a new line while println() does move the cursor to a new line.
+
 
 		 */
 
